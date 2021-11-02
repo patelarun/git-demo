2fcb50d7294fced7e002792dd4f897e83e102358
Arun <arunpwrk@gmail.com>
2021-11-02 11:29:28 +0530

    Add routing example with nested routing

--- src/App.js
+++ src/App.js
@@ -1,11 +1,26 @@
 import './App.css';
 import Users from './Users';
+import Todo from './Todo';
+import Counter from './Counter';
+import NestedRouting from './NestedRouting';
+import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
 
 function App(props) {
   return (
-    <div className="App">
-      <Users />
-    </div>
+    <Router>
+      <div>
+        <Link to="/">Home</Link>
+      </div>
+      <Link to="/todo">Todo</Link>
+      <Link to="/counter">Counter</Link>
+      <Link to="/nested-router">Nested Router</Link>
+      <div className="App">
+        <Route exact path="/" component={Users} />
+        <Route path="/todo" component={Todo} />
+        <Route path="/counter" component={Counter} />
+        <Route path="/nested-router" component={NestedRouting} />
+      </div>
+    </Router>
   );
 }
 
---
+++ src/NestedRouting/index.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import { Link, Route } from 'react-router-dom';
+
+const NestedRoute = (props) => console.log(props) || <p>{props.match.params.id}</p>;
+
+export default function NestedRouting () {
+  return (
+    <div>
+    <div>
+      <h1>Welcome to Contact Page</h1>
+      <strong>Select contact Id</strong>
+      <ul>
+        <li>
+          <Link to="/nested-router/1">Contacts 1 </Link>
+        </li>
+        <li>
+          <Link to="/nested-router/2">Contacts 2 </Link>
+        </li>
+        <li>
+          <Link to="/nested-router/3">Contacts 3 </Link>
+        </li>
+        <li>
+          <Link to="/nested-router/4">Contacts 4 </Link>
+        </li>
+      </ul>
+      <Route path="/nested-router/:id" component={NestedRoute} />
+    </div>
+    </div>
+  );
+}
